52f36beb1da33bbd1208eeda1f0da09d4609cc62
LJP-TW <accr94238@gmail.com>
2021-01-29 15:13:33 +0800

    Add HitconCTF-2020

---
+++ HitconCTF-2020/Tenet/struct.h
@@ -0,0 +1,30 @@
+struct user_regs_struct
+{
+    unsigned long long int r15;
+    unsigned long long int r14;
+    unsigned long long int r13;
+    unsigned long long int r12;
+    unsigned long long int rbp;
+    unsigned long long int rbx;
+    unsigned long long int r11;
+    unsigned long long int r10;
+    unsigned long long int r9;
+    unsigned long long int r8;
+    unsigned long long int rax;
+    unsigned long long int rcx;
+    unsigned long long int rdx;
+    unsigned long long int rsi;
+    unsigned long long int rdi;
+    unsigned long long int orig_rax;
+    unsigned long long int rip;
+    unsigned long long int cs;
+    unsigned long long int eflags;
+    unsigned long long int rsp;
+    unsigned long long int ss;
+    unsigned long long int fs_base;
+    unsigned long long int gs_base;
+    unsigned long long int ds;
+    unsigned long long int es;
+    unsigned long long int fs;
+    unsigned long long int gs;
+};
